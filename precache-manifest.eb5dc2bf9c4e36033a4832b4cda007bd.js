f4cc64631374aa6867c8721102a949543bbb1d24
Chetn11 <cnpatil00@gmail.com>
2023-11-22 13:46:43 +0530

    Updates

---
+++ precache-manifest.eb5dc2bf9c4e36033a4832b4cda007bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b4ef85fc8852d80e3df2b9e6868e099",
+    "url": "/stop-watch/index.html"
+  },
+  {
+    "revision": "130b26908a010d5f78fa",
+    "url": "/stop-watch/static/css/main.e6dcf2b1.chunk.css"
+  },
+  {
+    "revision": "f1ceeae124cc2f12f13b",
+    "url": "/stop-watch/static/js/2.7cc8e56a.chunk.js"
+  },
+  {
+    "revision": "130b26908a010d5f78fa",
+    "url": "/stop-watch/static/js/main.04d762d0.chunk.js"
+  },
+  {
+    "revision": "51b532d76858aee00f21",
+    "url": "/stop-watch/static/js/runtime~main.554d4035.js"
+  }
+]);
